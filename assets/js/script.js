848b9e27bc0a4e48d7be5398f8d08d861d127d6d
Huddy2022 <mlvnhud@aol.com>
2022-11-08 21:25:31 +0000

    add basic functions to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+runGame()
+
+checkAnswer()
+
+calucalteCorrectAnswer()
+
+increment+Score()
+
+incrment+WrongAnswer()
+
+displaySportQuestion()
+
+displayMoviesQuestion()
+
+displayGeographyQuestion()
+
+displayHisotryQuestion()

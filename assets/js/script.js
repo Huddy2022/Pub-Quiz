94179257086a348b247e5ca4a1ace4f7ce085f24
Huddy2022 <mlvnhud@aol.com>
2022-11-11 21:30:09 +0000

    add sports questions, option answers and correct answer to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,12 +23,75 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 function runGame(gameType) {
 
-    let sportsQuestion = ["Who has won more Grand Slams, Roger Federer or Serena Williams?", "Which country won the 2019 Rugby World Cup?", "How many F1 championships has Lewis Hamilton won?", "How many balls are in total are there on the table at the start of a game of snooker, including the white?", "What colours are the five Olympic rings?", "What is Usain Bolts world record time for 100m?", "Which team did Kobe Bryant spend his basketball career with?","Which country did F1 legend Ayrton Senna come from?", "Who has scored the most Premier League hat-tricks?", "Who did Cristiano Ronaldo make his Premier League debut against in 2003?"];
-    let print = document.getElementById('operand1')
+    let sportsQuestion = [
+        { question: "Which country won the 2019 Rugby World Cup?",
+          optionA: "South Africa",
+          optionB: "England",
+          optionC: "Wales",
+          correctOption: "optionA"
+         },
+         { question: "How many F1 championships has Lewis Hamilton won?",
+           optionA: "4",
+           optionB: "2",
+           optionC: "7",
+           correctOption: "optionC"
+        },
+        { question: "How many balls are in total are there on the table at the start of a game of snooker, including the white?",
+           optionA: "20",
+           optionB: "22",
+           optionC: "18",
+           correctOption: "optionB"
+        },
+        { question:"Who were Man Utd playing when Eric Cantona leaped into the crowd and kicked a fan?",
+           optionA: "Crystal Palace",
+           optionB: "Everton",
+           optionC: "Liverpool",
+           correctOption: "optionA"
+        },
+        { question: "What is Usain Bolts world record time for 100m?",
+           optionA: "10 seconds",
+           optionB: "9.58 seconds",
+           optionC: "9.2 seconds",
+           correctOption: "optionB"
+        },
+        { question: "Which team did Kobe Bryant spend his basketball career with?",
+           optionA: "Chicago Bulls",
+           optionB: "New York Knicks",
+           optionC: "LA Lakers",
+           correctOption: "optionC"
+        },
+        { question: "Which country did F1 legend Ayrton Senna come from?",
+           optionA: "Brasil",
+           optionB: "Argentina",
+           optionC: "Columbia",
+           correctOption: "optionA"
+        },
+        { question: "Who has scored the most Premier League hat-tricks?",
+           optionA: "Sergio Agüero",
+           optionB: "Alan Shearer",
+           optionC: "Robbie Fowler",
+           correctOption: "optionA"
+        },
+        { question:  "Who did Cristiano Ronaldo make his Premier League debut against in 2003?",
+           optionA: "Manchester City",
+           optionB: "Bolton Wanderers",
+           optionC: "Newcastle",
+           correctOption: "optionB"
+        },
+        { question:"Who did Conor McGregor fight in a one-off boxing match in 2017?",
+           optionA: "Antony Joshua",
+           optionB: "Tyson Furry",
+           optionC: "Floyd Mayweather",
+           correctOption: "optionC"
+        },
+             ];
+
+    let print = document.getElementById('display-question');
+    let sQuestion = sportsQuestion['question'];
 
     if(gameType === "Sports") {
-        let randomSportsQuestion= Math.floor(Math.random() * sportsQuestion.length);
-        print.innerHTML = sportsQuestion[randomSportsQuestion];
+        let randomSportsQuestion= Math.floor(Math.random() * sQuestion.length);
+        print.innerHTML = sQuestion[randomSportsQuestion];
     }
     else {
         alert(`unkown game type ${gameType}`);
